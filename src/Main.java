3789c14148f8a5b1aeb3b22e2863bd66397a25c3
1097101 <1097101@apps.nsd.org>
2024-04-17 09:31:02 -0700

    pushing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
+		testing();
+	}
+	
+	public static void testing() {
 		Word testWord = new Word("Akash");
 		System.out.println(testWord);
 		System.out.println(testWord);
